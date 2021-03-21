cad71848b9cee7ce0596b401a1ae84bc1a76228b
Ayush Ashish <ayushashish0911@gmail.com>
2021-03-21 19:09:39 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react"
+import "./App.css"
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            date: new Date(),
+            visible: true
+        }
+    }
+    componentDidMount() {
+        document.body.style.backgroundColor = "#EBE0CA"
+        setInterval(() => {
+            this.setState(prevState =>({
+                date: new Date(),
+                visible: !(prevState.visible)
+              })
+        )}, 1000)
+    }
+    render() {
+        return (
+            <div className={this.state.visible?"enclosing-div":"disappear"}>
+                <p className="date-element">{this.state.date.toDateString()}</p>
+                <h1 className="time-element">{this.state.date.toTimeString().split(" ",1)}</h1>
+            </div>
+        )
+    }
 }
-
-export default App;
+export default App
